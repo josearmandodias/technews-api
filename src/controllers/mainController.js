a9e15648029dc2c6e8c1ebfc4b2b2a8d1d697e19
josearmandodias <jadlop@protonmail.com>
2023-05-21 16:24:40 +0200

    :rocket: feat: html page for home with dynamic subject choices for API request

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -3,24 +3,29 @@ const cheerio = require('cheerio');
 
 const mainController = {
 
-  home: async (req, res) => {
-      res.send('Welcome on my tech news api');
-  },
-
   ai: async (req, res) => {
+    const subject = req.params.subject;
 
     const newspapers = [
       {
         name: "technologyreview",
-        url: "https://www.technologyreview.com/"
+        url: "https://www.technologyreview.com/",
+        base: ''
       },
       {
         name: "nytimes",
-        url:"https://www.nytimes.com/international/section/technology"
+        url:"https://www.nytimes.com/international/section/technology",
+        base: ''
       },
       {
-        name: 'wired',
-        url: 'https://www.wired.com/category/ideas/'
+        name: 'theguardian',
+        url: 'https://www.theguardian.com/uk/technology',
+        base: 'https://www.theguardian.com/'
+      },
+      {
+        name: 'hackernews',
+        url: 'https://news.ycombinator.com/',
+        base: ''
       }
     ]
     
@@ -33,16 +38,16 @@ const mainController = {
         const data = await response.text();
         const $ = cheerio.load(data);
 
-        $('a:contains("AI")', data).each(function () {
+        $(`a:contains("${subject}")`, data).each(function () {
           const title = $(this).text();
           const url = $(this).attr('href');
+
           news.push({
             title,
-            url: `https://${newspaper.name}.com` + url
+            url: newspaper.base + url
           });
         });
       }
-
     } catch(e) {
       throw new Error(e);
     }
--- src/router.js
+++ src/router.js
@@ -3,7 +3,10 @@ const mainController = require('./controllers/mainController');
 
 const router = Router();
 
-router.get('/', mainController.home);
-router.get('/ai', mainController.ai);
+router.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+router.get('/:subject', mainController.ai);
 
 module.exports = router;
